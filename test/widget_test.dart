ef8b816184ccef60e309daca877e58ecb0030ce6
anudaw <anuda@live.com>
2020-02-02 12:12:14 +0530

    changes to ready app for play store deployment

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:saisa_live_app/pages/main.dart';
+import 'package:saisa_live_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
